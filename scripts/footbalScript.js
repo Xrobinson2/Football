5074173fd9b0eed0fb3c30a3b81385cefe97291c
Xavier Robinson <x.robinson5041@outlook.com>
2024-05-17 16:25:04 -0400

    worked on script

--- scripts/footbalScript.js
+++ scripts/footbalScript.js
@@ -12,14 +12,64 @@ let teams = [
 
 
 
-const teamList = document.getElementById("teamList")
+
+
+const teamDropdown = document.getElementById("teamsDropdown");
+const submitButton = document.getElementById("submitButton");
+const outputTextBox = document.getElementById("outputTextBox");
+const outputImage = document.getElementById("outputImage");
+
 
 window.onload = function(){
+    loadTeams();
+    
+    submitButton.onclick = onSubmitButtonClicked;
+ 
+};
+
+window.onload = function(){
+  
+  const teamList = document.getElementById("teamList")
 
 let teamLength = teams.length
 for(let i = 0; i < teamLength; i++ ){
   let theOption = new Option(teams[i].name, teams[i].code)
   teamList.appendChild(theOption);
 }
+ 
+teamList.onchange = onTeamSelectionChanged
+}
+
+function onTeamSelectionChanged(){
+  let selectedValue = teamList.value;
+}
+
+
+
+
+function loadTeams(){
+    let blankOption = new Option("Select a Team", "");
+    teamDropdown.append(blankOption);
+    for(let team of teams){
+        let theOption = new Option(team.name, team.code);
+        teamDropdown.append(theOption);
+    }
+}
+
+function onSubmitButtonClicked(){
+    //console.log(teamDropdown.value);
+    if(teamDropdown.value == ""){
+        outputTextBox.innerHTML = "";
+        outputImage.setAttribute("src","");
+    } else {
+
+        for(let team of teams){
+            if(teamDropdown.value == team.code){
+                outputTextBox.innerHTML = 
+                `You selected ${team.name} (${team.code}) who play in ${team.plays}`;
+                outputImage.setAttribute("src", `images/${team.code}.png`);
+            }
+        }
+    }
 
 }
